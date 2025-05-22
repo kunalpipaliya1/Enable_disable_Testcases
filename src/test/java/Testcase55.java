8e09383fd3073726f82e44029cfc9a8aeb933e38
Kunal pipaliya <Kunal.pipaliya92@gmail.com>
2025-05-22 17:33:24 +0530

    How to enable_disble test cases

---
+++ src/test/java/Testcase55.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.*;
+public class Testcase55 {
+	
+	@Test
+	public void MobileLogin() {
+		System.out.println("Mobile login  Passed....!!!");
+	}
+	
+	@Test
+	public void Weblogin() {
+		System.out.println("Web login Passed....!!!");
+	}
+	
+	@Test
+	public void APIlogin() {
+		System.out.println("API login Passed....!!!");
+	}
+
+}
